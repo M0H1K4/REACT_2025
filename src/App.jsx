108175f48e6f07d0bd95852bc10c3e8b09de23b8
M0H1K4N <nejer911@gmail.com>
2025-01-21 18:38:40 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import reactImg from './assets/react-core-concepts.png'
+
+
+function HeaderComponent() {
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept(){
+  
+}
+
+function App() {
+  return (
+    <div>
+      <HeaderComponent />
+      <main>
+        <section id='core-concepts'></section>
+        <h2>Core Concepts</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
